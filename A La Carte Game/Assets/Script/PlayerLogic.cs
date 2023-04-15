a73143244b8064a585b469962006e7bb1ad32a5f
AanTp21 <ananda21062002@gmail.com>
2023-04-15 16:56:09 +0700

    logic player and camera

--- A La Carte Game/Assets/Script/PlayerLogic.cs
+++ A La Carte Game/Assets/Script/PlayerLogic.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerLogic : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
