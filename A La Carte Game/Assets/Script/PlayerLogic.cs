0316e63bb81c9e2d49e13c626fc1300bfea37d7f
AanTp21 <ananda21062002@gmail.com>
2023-05-22 19:46:47 +0700

    Penambahan efek asap

--- A La Carte Game/Assets/Script/PlayerLogic.cs
+++ A La Carte Game/Assets/Script/PlayerLogic.cs
@@ -9,9 +9,11 @@ public class PlayerLogic : MonoBehaviour
     private Vector3 posisiAwal;
     public Transform start;
     public Transform finish;
+    public GameObject kotak;
     // Start is called before the first frame update
     void Start()
-    {     
+    {
+        
         rb = GetComponent<Rigidbody>();
         posisiAwal = new Vector3(start.position.x + 1.5f,start.position.y + 2.5f ,start.position.z -1f);
         transform.position = posisiAwal;
@@ -23,17 +25,20 @@ public class PlayerLogic : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(-1 *horizontalInput , 0f,-1 * verticalInput );
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (  Input.GetKey(KeyCode.LeftShift))
         {
             rb.AddForce(movement * speed);
         }
-        if (transform.position.y <= 3.55 || Input.GetKey(KeyCode.R)) {
-
+        if (transform.position.y <= 3.5f || Input.GetKey(KeyCode.R)) {
             transform.position = posisiAwal;
         }
         if(transform.position.z <= finish.position.z) {
+            
+        
             print("sudah finish bro");
+            
         }
         rb.AddForce(movement);
     }
+    
 }
